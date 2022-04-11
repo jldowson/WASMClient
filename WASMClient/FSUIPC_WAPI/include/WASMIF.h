4b0be76cd2cf6853c105ed2d6fc579c00ccfd94f
John Dowson <jldowson@gmail.com>
2022-04-11 16:29:23 +0200

    WASM updated

--- WASMClient/FSUIPC_WAPI/include/WASMIF.h
+++ WASMClient/FSUIPC_WAPI/include/WASMIF.h
@@ -9,7 +9,7 @@
 #include "ClientDataArea.h"
 #include "CDAIdBank.h"
 
-#define WAPI_VERSION			"0.5.6"
+#define WAPI_VERSION			"0.5.7"
 
 using namespace ClientDataAreaMSFS;
 using namespace CDAIdBankMSFS;
