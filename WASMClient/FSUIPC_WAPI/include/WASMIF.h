dc07d7555c78b29584fdbae10e17cf60d33e13c5
John Dowson <jldowson@gmail.com>
2022-10-20 11:18:07 +0200

    Updated to VS2022. WAPI updated to 0.9.1

--- WASMClient/FSUIPC_WAPI/include/WASMIF.h
+++ WASMClient/FSUIPC_WAPI/include/WASMIF.h
@@ -9,7 +9,7 @@
 #include "ClientDataArea.h"
 #include "CDAIdBank.h"
 
-#define WAPI_VERSION			"0.9.0"
+#define WAPI_VERSION			"0.9.1"
 
 using namespace ClientDataAreaMSFS;
 using namespace CDAIdBankMSFS;
